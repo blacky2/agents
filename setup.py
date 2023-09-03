cab231bb222be8ca8ed60d5aa9f3e879d62eb2f3
blacky2178 <schwarz-philipp@t-online.de>
2023-09-03 14:11:49 +0200

    fix

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.0.1',
     install_requires=[
         'numpy==1.25',
-        'gymnasium==0.29'
-        'minari==1.1',
-        'd4rl==1.1'
+        'gymnasium==0.29',
+        'minari==0.4',
+        'd4rl==1.1',
     ],
 )
