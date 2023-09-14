1c14b269b2ed91c902aaf86b117e9aaaa8cd367f
blacky2 <schwarz-philipp@t-online.de>
2023-09-14 21:08:09 +0200

    simple ql impl

--- agents/ql.py
+++ agents/ql.py
@@ -3,27 +3,56 @@ import gymnasium as gym
 
 
 def train(agent, env, episodes, seed):
-
-    # TODO logging and stuff
-
     for episode_idx in range(episodes):
         obs, info = env.reset(seed=seed)
 
         terminated, truncated = False, False
+        step_idx = 0
         while not (terminated or truncated):
             action = agent.action(obs)
             obs, reward, terminated, truncated = env.step(action)
 
             # inform agent about the reward
+            agent.update(obs, reward, terminated, truncated)
+            step_idx += 1
+
+        yield episode_idx
+
+
+def evaluate(agent, env, episodes, seed, ):
+    for episode_idx in range(episodes):
+        obs, info = env.reset(seed=seed)
+
+        terminated, truncated = False, False
+        step_idx = 0
+        while not (terminated or truncated):
+            action = agent.action(obs)
+            obs, reward, terminated, truncated = env.step(action)
+
+            # inform agent about the reward
+            agent.update(obs, reward, terminated, truncated)
+            step_idx += 1
+
+        yield episode_idx
 
 
 class QLAgent:
 
-    def __init__(self, action_space, observation_space):
+    def __init__(self, learning_rate, discount, action_space, observation_space):
         self.qtable = np.zeros((observation_space.n, action_space.n))
+        self.learning_rate = learning_rate
+        self.discount = discount
 
     def action(self, observation):
         return np.argmax(self.qtable[observation, :])
+
+    def update(self, obs, action, new_obs, reward, terminated, truncated):
+        if terminated:
+            target = reward
+        else:
+            target = reward + self.discount * np.max(self.qtable[new_obs, :])
+        q_value = self.qtable[obs, action]
+        self.qtable[obs, action] = (1 - self.learning_rate) * q_value + self.learning_rate * target
 
 
 if __name__ == '__main__':
@@ -37,7 +66,8 @@ if __name__ == '__main__':
     try:
         agent = QLAgent(env.action_space, env.observation_space)
 
-        train(agent, env, 100, seed=1889)
+        for episode_idx in train(agent, env, 100, seed=1889):
+            pass
         a = 1
         print('test ql', a)
     finally:
